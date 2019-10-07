cac2e3aed5a39bbb137dc7a59b55bc4015a0515c
MariyamElshrief <mariyam.elshrief17@stjohns.edu>
2019-10-06 20:51:13 -0400

    Updated

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+#just get the current directory.
+basedir = os.path. abspath (os.path.dirname(__file__))
+
+# Get the database URL from the environment variable, if available
+# otherwise give the database a default name 'app.db'
+class Config (object):
+    SQLAlchemy_DATABASE_URI = os.environ.get ('DATABASE_URL') or \
+    'sqlite:///' + os.path.join (basedir, 'app,db')
+    SQLAlchemy_TRACK_MODIFICATIONS = False
+
+    
